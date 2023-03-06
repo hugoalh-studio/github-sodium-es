a2dc1f0050ba8453bdebbf248fbe4a46892ffd92
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-03-06 11:38:42 +0800

    20230306B

--- dist/seal.d.ts
+++ dist/seal.d.ts
@@ -1,5 +1,6 @@
 /**
  * @function seal
+ * @description Encrypt value to GitHub secret value.
  * @param {string} publicKey Organization's or repository's public key, which need for encrypt value to secret value before create or update a secret.
  * @param {string} value Value that need to encrypt as secret value.
  * @returns {string} An encrypted GitHub secret value.
