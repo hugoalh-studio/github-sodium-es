1d38effc54d88a64acc7280186412881c36fcd95
hugoalh <hugoalh@gmail.com>
2026-09-15 14:32:22 +0800

    Fix CLI

--- tweetsodium/cli.ts
+++ tweetsodium/cli.ts
@@ -1,4 +1,4 @@
-import { exit } from "node:process";
+import process from "node:process";
 import {
 	parseArgs,
 	styleText
@@ -7,20 +7,14 @@ import { GitHubSodiumSealer } from "../tweetsodium.ts";
 if (!import.meta.main) {
 	throw new Error(`This entrypoint is for command line only!`);
 }
-addEventListener("unhandledrejection", (event: PromiseRejectionEvent): void => {
-	event.preventDefault();
-	let message: string;
-	if (event.reason instanceof Error) {
-		message = event.reason.message;
-		if ((event.reason.stack ?? "").length > 0) {
-			message += `\n${event.reason.stack}`;
-		}
-	} else {
-		message = String(event.reason);
+process.addListener("uncaughtException", (error: Error): void => {
+	let message: string = error.message;
+	if ((error.stack ?? "").length > 0) {
+		message += `\n${error.stack}`;
 	}
 	console.error(`${styleText(["red"], "ERROR", { validateStream: false })}\t${message}`);
-	exit(1);
-}, { capture: true });
+	process.exit(1);
+});
 const { positionals } = parseArgs({ allowPositionals: true });
 if (positionals.length !== 2) {
 	throw new Error(`Invalid arguments length! Expect: 2, Current: ${positionals.length}.`);
